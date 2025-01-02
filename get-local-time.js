082aa47b58161b610b13e9a114716899fda1ea49
Magnus Smedberg <alton@altonveonion.com>
2025-01-02 19:03:09 +0100

    Update get-local-time.js
    Add more examples
    
    Signed-off-by: Magnus Smedberg <alton@altonveonion.com>

--- get-local-time.js
+++ get-local-time.js
@@ -2,11 +2,24 @@
   Gets the time in the local timezone and format
 */
 
-let sys = await Homey.system.getInfo();
-let time = new Date().toLocaleString(sys.country, {
-timeZone: sys.timezone,
-hour: '2-digit',
-minute: '2-digit'
-});
+const sys = await Homey.system.getInfo();
 
-return time;
+// Local date
+const date = new Date().toLocaleDateString(sys.language + "-" + sys.country, { timeZone: sys.timezone });
+log(date)
+
+// Local date and time
+const datetime = new Date().toLocaleString(sys.language + "-" + sys.country, { timeZone: sys.timezone });
+log(datetime)
+
+// Local time
+const time1 = new Date().toLocaleTimeString(sys.language + "-" + sys.country, { timeZone: sys.timezone });
+log(time1)
+
+// Local time with more options
+const time2 = new Date().toLocaleString(sys.country, {
+  timeZone: sys.timezone,
+  hour: '2-digit',
+  minute: '2-digit'
+});
+log(time2);
