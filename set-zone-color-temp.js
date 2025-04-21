a7ef88b8355ee8e966df47be18cbb87eb3869cbf
Magnus Smedberg <alton@altonveonion.com>
2025-04-21 17:34:05 +0200

    Create set-zone-color-temp.js
    Signed-off-by: Magnus Smedberg <alton@altonveonion.com>

---
+++ set-zone-color-temp.js
@@ -0,0 +1,99 @@
+/*
+  Set the color temperature of all supported lights in the specified zones either with an absolute or relative percentage value.
+  If no zone is specified it changes the color temperature all lights.
+  Updated: 2025-04-21
+
+  Argument:
+    The color temperature (in percentage) and zones (optional) separated by |.
+    Examples:
+    '60|Kitchen|Hallway'  absolute value
+    '+20|Kitchen|Hallway' relative value
+    '-20|Kitchen|Hallway' relative value
+
+  Variables:
+    duration: The duration in seconds for the transition to the new color temperature.
+
+    include_subzones: Whether the script should include subzones or not.
+
+    delay_between_devices: The delay between commands to devices in milliseconds.
+
+*/
+
+const duration = 0.5;
+const include_subzones = true;
+const delay_between_devices = 0;
+
+
+
+// ====================================== //
+
+
+if (!args[0]) {
+  throw new Error("Must be run with an argument");
+}
+
+// Parse argument
+let relative = false;
+args = args[0].split("|");
+let temp = parseInt(args[0]);
+if (temp || temp === 0) {
+  if (args[0].charAt(0) === "+" || args[0].charAt(0) === "-") relative = true;
+  args.shift();
+  temp = Math.min(1, Math.max(-1, temp / 100));
+} else {
+  throw new Error("Not a number");
+}
+
+const devices = await Homey.devices.getDevices();
+const zones = await Homey.zones.getZones();
+
+let allZonesId = [];
+
+//Get the id's of all zones
+if (include_subzones) for (const a of args) {
+  allZonesId = allZonesId.concat((await getSubZones(a)).map(i => i.id));
+}
+
+for (const device of Object.values(devices)) {
+  // Check if the device is a light
+  if (device.class !== 'light' && device.virtualClass !== 'light') continue;
+
+  // Simple zone matching
+  if (args.length && !include_subzones && !args.includes(zones[device.zone].name)) continue;
+
+  // Advanced zone matching
+  if (args.length && include_subzones && !allZonesId.includes(device.zone)) continue;
+
+  if (device.capabilities.includes('light_temperature')) {
+    let val = (relative ? device.capabilitiesObj.light_temperature.value + temp : temp);
+    //device.setCapabilityValue('light_temperature', val);
+    setDeviceProperty(device.id, 'temperature', val, duration);
+    if (delay_between_devices > 0) await wait(delay_between_devices);
+  }
+}
+
+
+async function getSubZones(startZone) {
+  let zones = await Homey.zones.getZones();
+  let result = _.filter(zones, { 'name': startZone });
+  if (!result[0]) return undefined;
+  let id = _.map(result, 'id');
+
+  let arr = [];
+  while (id.length) {
+    arr = _.filter(zones, function (z) {
+      return id.includes(z.parent);
+    });
+    result = _.concat(result, arr);
+    id = _.map(arr, 'id');
+  }
+  return result;
+}
+
+async function setDeviceProperty(device, type, value, duration = 0) {
+  await Homey.flow.runFlowCardAction({
+    id: `homey:device:${device}:${type}`,
+    duration: duration,
+    args: { [type]: value }
+  });
+}
