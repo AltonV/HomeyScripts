e88bc22c241f74e59d085b09138dd44c853c8e1e
Magnus Smedberg <alton@altonveonion.com>
2025-05-24 21:48:01 +0200

    Tweaks
    * No need to fetch individual devices and zones in the loop.
    * Use the localised name of the capability
    * Added some comments

--- list-zone-activity-reason.js
+++ list-zone-activity-reason.js
@@ -1,21 +1,25 @@
 // List all active zones and the reason they are active.
+
 const zones = await Homey.zones.getZones();
+const devices = await Homey.devices.getDevices();
+
 for (const zone of Object.values(zones)) {
-  if (!zone.active) continue;
-  log("=".repeat(5), zone.name);
-  for (const origin of zone.activeOrigins) {
+  if (!zone.active) continue; // Skip inactive zones
+
+  log("=".repeat(5), zone.name); // Print zone name
+  for (const origin of zone.activeOrigins) { // Loop through reasons why zone i active
     const match = origin.match(/(?<=homey:)([^:]*):([^:]*):*(.*)/);
-    if (match[1] === "device") {
+    if (match[1] === "device") { // If the reason is a device
       try {
-        const device = await Homey.devices.getDevice({ id: match[2] });
-        log("Device:", device.name, "|", match[3]);
+        // Print the name of the device and the reason it is active
+        log("Device:", devices[match[2]].name, "|", devices[match[2]].capabilitiesObj[match[3]].title);
       } catch (e) {
         log(e);
       }
-    } else if (match[1] === "zone") {
+    } else if (match[1] === "zone") { // If the reason is a sub-zone
       try {
-        const originZone = await Homey.zones.getZone({ id: match[2] });
-        log("Zone:", originZone.name);
+        // Print the name of the sub-zone
+        log("Zone:", zones[match[2]].name);
       } catch (e) {
         log(e);
       }
